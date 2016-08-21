08e16e726cacfe16babbb560febef9b6dbe1dd6e
Eugeniy Belyaev <ieugenor@gmail.com>
2016-08-21 17:32:28 +0300

    ...

--- core/src/ru/belyaev/holdtheball/util/Strings.java
+++ core/src/ru/belyaev/holdtheball/util/Strings.java
@@ -10,7 +10,8 @@ public class Strings {
     public static final String BEST_TIME_STRING = "Best time: " + TIME_FORMAT;
     public static final String HOLD_THE_BALL_STRING = "Hold the ball";
     public static final String RETRY_STRING = "RETRY";
-    public static final String TIP_STRING = "Press and hold your finger\ninside the ball";
+    public static final String TIP_1_STRING = "Press and hold your finger inside the ball";
+    public static final String TIP_2_STRING = "Do not let the ball";
 
     public static String getTimeString(float time) {
         return String.format(Locale.US, TIME_STRING, time);
